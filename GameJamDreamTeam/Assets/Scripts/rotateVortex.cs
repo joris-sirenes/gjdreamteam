b6b7959a3148dac6e428dc480edbfc0799471828
joris-sirenes <simonet.joris@gmail.com>
2017-01-22 00:05:05 +0100

    Add files via upload

---
+++ GameJamDreamTeam/Assets/Scripts/rotateVortex.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class rotateVortex : MonoBehaviour {
+    float rotate;
+	// Use this for initialization
+	void Start () {
+        rotate = 5f;
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] vortexes = GameObject.FindGameObjectsWithTag("Vortex");
+        foreach (var vor in vortexes)
+        {
+            vor.transform.Rotate(0, 0, rotate);
+        }
+    }
+}
