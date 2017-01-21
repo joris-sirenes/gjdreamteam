27c25adb8d4803bfd64541787a62d5ade20f4062
thibautFarcin <thibaut.facrin@gmail.com>
2017-01-21 03:51:07 +0100

    Hero movement

---
+++ GameJamDreamTeam/Assets/Scripts/Player_Control.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        Jump();
+        Shoot();
+    }
+
+    void Movement()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * 4f * Time.deltaTime);
+        }else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(Vector2.left * 4f * Time.deltaTime);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Translate(Vector2.up * 4f * Time.deltaTime);
+        }
+    }
+
+    void Shoot()
+    {
+        bool shoot_R = Input.GetKeyDown(KeyCode.RightArrow);
+        bool shoot_L = Input.GetKeyDown(KeyCode.LeftArrow);
+        bool shoot_U = Input.GetKeyDown(KeyCode.UpArrow);
+        bool shoot_D = Input.GetKeyDown(KeyCode.DownArrow);
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        if (shoot_R || shoot_D|| shoot_L || shoot_U)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                if(shoot_U)
+                    weapon.direction = "UP";
+                if(shoot_R)
+                    weapon.direction = "RIGHT";
+                if (shoot_L)
+                    weapon.direction = "LEFT";
+                if (shoot_D) { 
+                    weapon.direction = "DOWN";
+                    transform.Translate(Vector2.up * 40f * Time.deltaTime );
+                }
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
+    }
+}
+
---
+++ GameJamDreamTeam/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Launch projectile
+/// </summary>
+public class WeaponScript : MonoBehaviour
+{
+    public string direction;
+
+    //--------------------------------
+    // 1 - Designer variables
+    //--------------------------------
+
+    /// <summary>
+    /// Projectile prefab for shooting
+    /// </summary>
+    public Transform shotPrefab;
+
+    /// <summary>
+    /// Cooldown in seconds between two shots
+    /// </summary>
+    public float shootingRate = 0.25f;
+
+    //--------------------------------
+    // 2 - Cooldown
+    //--------------------------------
+
+    private float shootCooldown;
+
+    void Start()
+    {
+        shootCooldown = 0f;
+    }
+
+    void Update()
+    {
+        if (shootCooldown > 0)
+        {
+            shootCooldown -= Time.deltaTime;
+        }
+    }
+
+    //--------------------------------
+    // 3 - Shooting from another script
+    //--------------------------------
+
+    /// <summary>
+    /// Create a new projectile if possible
+    /// </summary>
+    public void Attack(bool isEnemy)
+    {
+        if (CanAttack)
+        {
+            shootCooldown = shootingRate;
+
+            // Create a new shot
+            var shotTransform = Instantiate(shotPrefab) as Transform;
+
+            // Assign position
+            shotTransform.position = transform.position;
+
+            // The is enemy property
+            ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+            if (shot != null)
+            {
+                shot.isEnemyShot = isEnemy;
+            }
+
+            // Make the weapon shot always towards it
+
+            
+            MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
+            if (move != null)
+            {
+                if (direction.Equals("UP"))
+                {
+                    move.direction = this.transform.up;
+                    shot.gameObject.transform.Rotate(new Vector3(0, 0, 45));
+                }
+                if (direction.Equals("RIGHT"))
+                {
+                    move.direction = this.transform.right;
+                    shot.gameObject.transform.Rotate(new Vector3(0, 0, -45));
+                }
+                if (direction.Equals("LEFT"))
+                {
+                    move.direction = - this.transform.right;
+                    shot.gameObject.transform.Rotate(new Vector3(0, 0, 135));
+                }
+                if (direction.Equals("DOWN"))
+                {
+                    move.direction = -this.transform.up;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Is the weapon ready to create a new projectile?
+    /// </summary>
+    public bool CanAttack
+    {
+        get
+        {
+            return shootCooldown <= 0f;
+        }
+    }
+}
+
