5badd8f0abd04a0484e2dc83ef7c5622379b2cb7
Joris Simonet <Joris Simonet>
2017-01-22 11:45:59 +0100

    Add menu

---
+++ GameJamDreamTeam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public void ButtonMenu(Button button)
+    {
+        if(button.name == "Button_new")
+        {
+            Application.LoadLevel("niveau1bis");
+        }
+        else if(button.name == "Button_select")
+        {
+            Application.LoadLevel("MenuSelectLevel");
+        }
+        else if(button.name == "Button_quit")
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ GameJamDreamTeam/Assets/Scripts/MenuSelectScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MenuSelectScript : MonoBehaviour
+{
+
+    public void ButtonMenu(Button button)
+    {
+        if (button.name == "level1")
+        {
+            Application.LoadLevel("niveau1bis");
+        }
+        else if (button.name == "level2")
+        {
+            Application.LoadLevel("niveau2");
+        }
+        else if (button.name == "level3")
+        {
+            Application.LoadLevel("niveau3");
+        }
+        else if (button.name == "back")
+        {
+            Application.LoadLevel("MainMenu");
+        }
+    }
+}
